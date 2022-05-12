139342b1fe4bccda8c6e8472cfa4d9e91509a953
kunal <kunalbankoti9@gmail.com>
2022-05-12 17:13:30 +0530

    Add function for single round play
    The function takes player input against computer choice as arguments & compares both to produce result

--- myScript.js
+++ myScript.js
@@ -1,20 +1,26 @@
-let options=['rock','paper','scissors'];
+let options = ['rock', 'paper', 'scissors'];
 
-function playerPlay()
-{
-    let choice=prompt("Enter choice").toLowerCase();
-   choice=options.find((value)=>value==choice);
-   if(!choice)
-   {
-       alert("invalid input");
-       playerPlay();
-   }
-   return choice;
+function playRound(playerSelection, computerSelection) {
+
+    const indexOfPlayer = options.indexOf(playerSelection);
+    const indexOfComputer = options.indexOf(computerSelection);
+    const len = options.length;
+    return (indexOfPlayer + 1) % len == indexOfComputer ? false : true;
 
 }
-function computerPlay()
-{
-const index=Math.floor(Math.random()*3);
-return options[index];
+function playerPlay() {
+
+    let choice = prompt("Enter choice").toLowerCase();
+    choice = options.find((value) => value == choice);
+    if (!choice) {
+        alert("invalid input");
+        playerPlay();
+    }
+    return choice;
+
 }
-console.log(computerPlay());
+function computerPlay() {
+
+    const index = Math.floor(Math.random() * 3);
+    return options[index];
+}

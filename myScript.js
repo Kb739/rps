4f973275247e558b3c8201ce9e44864bfb0eb910
kunal <kunalbankoti9@gmail.com>
2022-05-12 17:54:21 +0530

    Implement game function which runs loop for n rounds

--- myScript.js
+++ myScript.js
@@ -1,25 +1,45 @@
 let options = ['rock', 'paper', 'scissors'];
 
+game(3);
+
+function game(rounds = 5) {
+
+    console.assert(rounds & 1, "rounds cannot be even");
+
+    let playerScore = 0,
+        computerScore = 0;
+
+    for (let i = 0; i < rounds; i++) {
+        playRound(playerPlay(), computerPlay()) ? playerScore++ : computerScore++;
+        console.log(`Player = ${playerScore} , Computer = ${computerScore}`);
+    }
+    playerScore > computerScore ? alert("you win") : alert("you loose");
+}
+
 function playRound(playerSelection, computerSelection) {
+    //returns true if player wins the round , false otherwise
 
     const indexOfPlayer = options.indexOf(playerSelection);
     const indexOfComputer = options.indexOf(computerSelection);
     const len = options.length;
     return (indexOfPlayer + 1) % len == indexOfComputer ? false : true;
-
 }
-function playerPlay() {
 
-    let choice = prompt("Enter choice").toLowerCase();
-    choice = options.find((value) => value == choice);
+function playerPlay() {
+    //prompts player to enter choice ,loops until valid choice is found
+
+    let choice = prompt("Enter choice");
     if (!choice) {
         alert("invalid input");
         playerPlay();
     }
-    return choice;
+    choice = options.find((value) => value == choice.toLowerCase()) || playerPlay();
 
+    return choice;
 }
+
 function computerPlay() {
+    //computer selects random value from the options array
 
     const index = Math.floor(Math.random() * 3);
     return options[index];

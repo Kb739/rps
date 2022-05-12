df5d7e5534ce3adb0a94350446be1d4bf82aae20
kunal <kunalbankoti9@gmail.com>
2022-05-12 16:49:26 +0530

    Implement playerPlay() function to execute player's turn

--- myScript.js
+++ myScript.js
@@ -1,4 +1,17 @@
 let options=['rock','paper','scissors'];
+
+function playerPlay()
+{
+    let choice=prompt("Enter choice").toLowerCase();
+   choice=options.find((value)=>value==choice);
+   if(!choice)
+   {
+       alert("invalid input");
+       playerPlay();
+   }
+   return choice;
+
+}
 function computerPlay()
 {
 const index=Math.floor(Math.random()*3);
